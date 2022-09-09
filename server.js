425cf729e0178623f220b4b442eee2b6eab5566d
hye-on <ain0103@naver.com>
2022-09-09 21:21:44 +0900

    todoapp

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const MongoClient =require('mongodb').MongoClient;
 app.set('view engine','ejs');
+app.use('/public', express.static('public'))
 var db;
 MongoClient.connect('mongodb+srv://ain0103:HGu5b3r1aIz0hQqs@cluster0.8s3c6l1.mongodb.net/?retryWrites=true&w=majority',function(에러,client){
 
@@ -31,11 +32,11 @@ MongoClient.connect('mongodb+srv://ain0103:HGu5b3r1aIz0hQqs@cluster0.8s3c6l1.mon
 //     응답.send('뷰티용품 쇼핑할 수 있는 페이지입니다.');
 // });
 app.get('/',(요청,응답)=>{//함수 안에 함수 = 콜백함수 -> 순사적으로 실행하고 싶을 때 쓴다.
-    응답.sendFile(__dirname+'/index.html')
+    응답.render('index.ejs');
 });
 
 app.get('/write',function(요청,응답){
-    응답.sendFile(__dirname+'/write.html')
+    응답.render('write.ejs')
     
 });
 
@@ -63,13 +64,33 @@ app.post('/add',function(요청,응답){
 app.get('/list',function(요청,응답){
     //db에 저장된 post 라는 컬렉션 안의 모든 데이터 꺼내기
     db.collection('post').find().toArray(function(에러,결과){
-        console.log(결과);
+       // console.log(결과);
         응답.render('list.ejs',{posts: 결과});
 
     });
 });
 
 app.delete('/delete',function(요청, 응답){
-    console.log(요청.body)
     
-})
+    console.log(요청.body)
+    요청.body._id = parseInt(요청.body._id);
+    //요청.body에 담겨온 게시물번호를 가진 글을 db에서 찾아서 삭제해주세요
+    db.collection('post').deleteOne(요청.body,function(에러,결과){
+        console.log('삭제완료');
+        응답.status(200).send({message : '성공했습니다.'});
+    })
+    
+});
+
+app.get('/detail/:id', function(요청, 응답){
+    db.collection('post').findOne({ _id : parseInt(요청.params.id) }, function(에러, 결과){
+      응답.render('detail.ejs', {data : 결과} );
+    })
+  });
+
+  app.get('/edit/:id',function(요청,응답){
+    
+    db.collection('post').findOne({ _id : parseInt(요청.params.id) }, function(에러, 결과){
+        응답.render('edit.ejs', { post : 결과 })
+      })
+});
